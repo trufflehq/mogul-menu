54fb9e17bae185252f09e9875e10856632ae6b87
Austin <austin@austinfay.com>
2022-07-11 16:03:29 -0600

    refac: is-live-info

--- components/is-live-info/is-live-info.tsx
+++ components/is-live-info/is-live-info.tsx
@@ -1,5 +1,5 @@
 import React from "https://npm.tfl.dev/react";
-import root from "https://npm.tfl.dev/react-shadow@19";
+import ScopedStylesheet from "https://tfl.dev/@truffle/ui@0.0.1/components/scoped-stylesheet/scoped-stylesheet.js";
 
 import Timer from "../timer/timer.tsx";
 
@@ -24,45 +24,43 @@ export default function IsLiveInfo(props) {
   const timeWatchedSeconds = 10;
 
   return (
-    <root.div className="live-info">
-      <link
-        rel="stylesheet"
-        href={new URL("is-live-info.css", import.meta.url).toString()}
-      />
-      <div
-        className="header"
-        style={{
-          "--background": highlightButtonBg ?? "var(--truffle-gradient)",
-        }}
-      >
-        {creatorName ? `${creatorName} is live!` : ""}
-      </div>
-      <div className="info">
-        <div className="message">
-          {creatorName
-            ? hasChannelPoints && hasBattlePass
-              ? `Earn channel points and XP by watching ${creatorName} during the stream`
-              : hasChannelPoints
-              ? `Earn channel points by watching ${creatorName} during the stream`
-              : hasBattlePass
-              ? `Earn XP by watching ${creatorName} during the stream`
-              : "Channel Points and XP not currently enabled"
-            : "Channel Points and XP not currently enabled"}
+    <ScopedStylesheet url={new URL("is-live-info.css", import.meta.url)}>
+      <div className="live-info">
+        <div
+          className="header"
+          style={{
+            "--background": highlightButtonBg ?? "var(--truffle-gradient)",
+          }}
+        >
+          {creatorName ? `${creatorName} is live!` : ""}
         </div>
-        <div className="grid">
-          {(hasChannelPoints || hasBattlePass || true) && (
-            // <Timer timerSeconds={timeWatchedSeconds} message={"Time watched"} />
-            <Timer timerSeconds={10} message={"Time watched"} />
-          )}
-          {(hasChannelPoints || hasBattlePass || true) && (
-            // <Timer
-            //   timerSeconds={secondsRemaining}
-            //   message={"Time until reward"}
-            // />
-            <Timer timerSeconds={10} message={"Time until reward"} />
-          )}
+        <div className="info">
+          <div className="message">
+            {creatorName
+              ? hasChannelPoints && hasBattlePass
+                ? `Earn channel points and XP by watching ${creatorName} during the stream`
+                : hasChannelPoints
+                ? `Earn channel points by watching ${creatorName} during the stream`
+                : hasBattlePass
+                ? `Earn XP by watching ${creatorName} during the stream`
+                : "Channel Points and XP not currently enabled"
+              : "Channel Points and XP not currently enabled"}
+          </div>
+          <div className="grid">
+            {(hasChannelPoints || hasBattlePass || true) && (
+              // <Timer timerSeconds={timeWatchedSeconds} message={"Time watched"} />
+              <Timer timerSeconds={10} message={"Time watched"} />
+            )}
+            {(hasChannelPoints || hasBattlePass || true) && (
+              // <Timer
+              //   timerSeconds={secondsRemaining}
+              //   message={"Time until reward"}
+              // />
+              <Timer timerSeconds={10} message={"Time until reward"} />
+            )}
+          </div>
         </div>
       </div>
-    </root.div>
+    </ScopedStylesheet>
   );
 }
