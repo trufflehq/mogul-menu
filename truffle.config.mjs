85e6ca8a2dacfb5ff0515609129fe6dd5419e978
Austin <austin@austinfay.com>
2022-07-12 14:29:18 -0600

    chore: version bump

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@dev/mogul-menu",
-  version: "0.1.6",
+  version: "0.1.8",
   // apiUrl: 'https://mycelium.truffle.vip/graphql',
   apiUrl: "https://mycelium.staging.bio/graphql",
   // apiUrl: 'http://localhost:50420/graphql'
