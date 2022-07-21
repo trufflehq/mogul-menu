0247ff9e9101bd70a238bfd27d0265a2f6ca99b2
Austin Hallock <austinhallock@gmail.com>
2022-07-20 19:53:00 -0600

    tweaks for prod

--- components/season-pass/season-pass.tsx
+++ components/season-pass/season-pass.tsx
@@ -20,7 +20,7 @@ import { useDialog } from "../dialog-container/dialog-service.ts";
 
 import UnlockedEmoteDialog from "../unlocked-emote-dialog/unlocked-emote-dialog.tsx";
 import RedeemableDialog from "../redeemable-dialog/redeemable-dialog.tsx";
-import ItemDialog from "../item-dialog/item-dialog.js";
+import ItemDialog from "../item-dialog/item-dialog.tsx";
 
 import ImageByAspectRatio from "https://tfl.dev/@truffle/ui@~0.1.0/components/legacy/image-by-aspect-ratio/image-by-aspect-ratio.tsx";
 import Icon from "https://tfl.dev/@truffle/ui@~0.1.0/components/legacy/icon/icon.tsx";
