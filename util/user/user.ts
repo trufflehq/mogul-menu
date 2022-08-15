4fb3add77c32e0a2e8d64d12365de839a9fd8ab3
Riley Miller <rmllr000@gmail.com>
2022-08-15 12:00:29 -0600

    fix: member only if theres a phone/email since we autopopulate the username

--- util/user/user.ts
+++ util/user/user.ts
@@ -1,3 +1,3 @@
-import { MeUser } from '../../types/mod.ts'
+import { MeUser } from "../../types/mod.ts";
 
-export const isMemberMeUser = (user: MeUser) => user && (user.name || user.email || user.phone);
+export const isMemberMeUser = (user: MeUser) => user && (user.email || user.phone);
