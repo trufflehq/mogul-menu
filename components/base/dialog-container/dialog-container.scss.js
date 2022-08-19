33f76d9e904580524d9745d6470832671ef87cfc
shanecranor <shanecranor@gmail.com>
2022-08-19 15:52:11 -0600

    fixed dialog positioning

--- components/base/dialog-container/dialog-container.scss.js
+++ components/base/dialog-container/dialog-container.scss.js
@@ -2,11 +2,9 @@ import { scss } from "../../../deps.ts";
 
 export default scss`
 .c-dialog-container {
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute ;
   background-color: rgba(0, 0, 0, 0.8);
   display: flex;
   justify-content: center;
