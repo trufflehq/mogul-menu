0a429a527df24abd59494e98692c4eabfb0fce89
Riley Miller <rmllr000@gmail.com>
2022-08-21 20:29:08 -0600

    refac: nonfocus trap cleanup

--- components/settings/settings-page/settings-page.tsx
+++ components/settings/settings-page/settings-page.tsx
@@ -1,26 +1,17 @@
-import { React, useRef } from "../../../deps.ts";
+import { React } from "../../../deps.ts";
 import { usePageStack } from "../../../util/mod.ts";
 import MenuItem from "../../base/menu-item/menu-item.tsx";
 import Page from "../../base/page/page.tsx";
 import AccountDetailsPage from "../account-details-page/account-details-page.tsx";
 
 export default function SettingsPage() {
-  const $$settingsPageBackRef = useRef<HTMLDivElement>(null)
   const { pushPage } = usePageStack();
-  const { popPage } = usePageStack();
-
-  const onBack = () => {
-    console.log('on back', $$settingsPageBackRef.current)
-    popPage()
-    $$settingsPageBackRef.current?.focus()
-  }
 
   return (
-    <Page title="Settings" backIconRef={$$settingsPageBackRef}>
+    <Page title="Settings" >
       <MenuItem
         icon="accountCircle"
         onClick={() => pushPage(<AccountDetailsPage />)}
-        onBack={onBack}
       >
         Account details
       </MenuItem>
