16d94b5b1544919dc332ce54f5f219c064789298
Riley Miller <rmllr000@gmail.com>
2022-08-30 00:08:26 -0600

    chore: bring back config

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,11 +1,11 @@
 export default {
-  name: "@dev/mogul-menu",
-  version: "0.1.36",
+  name: "@truffle/mogul-menu",
+  version: "0.1.35",
   // staging
-  apiUrl: "https://mycelium.staging.bio/graphql",
+  //apiUrl: "https://mycelium.staging.bio/graphql",
 
   // prod
-  // apiUrl: "https://mycelium.truffle.vip/graphql",
+  apiUrl: "https://mycelium.truffle.vip/graphql",
 
   // localhost
   // apiUrl: 'http://localhost:50420/graphql'
