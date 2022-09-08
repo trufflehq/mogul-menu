e8830e8c184601612a6a668309481321d5765d85
Riley Miller <rmllr000@gmail.com>
2022-09-08 13:33:16 -0600

    refac: reduced meWithConnection query

--- shared/util/user-info/gql.ts
+++ shared/util/user-info/gql.ts
@@ -39,21 +39,7 @@ export const USER_INFO_QUERY = gql`
 export const ME_CONNECTIONS_QUERY = gql`
   query {
     me {
-      id
       name
-      email
-      phone
-      avatarImage {
-        cdn
-        prefix
-        ext
-        variations {
-          postfix
-          width
-          height
-        }
-        aspectRatio
-      }
       connectionConnection(sourceTypes: ["youtube", "twitch"]) {
         nodes {
           id
