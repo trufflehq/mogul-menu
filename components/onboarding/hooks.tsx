af39ae314350d5cf6e036e9391746404619514cb
Riley Miller <rmllr000@gmail.com>
2022-09-12 11:22:40 -0600

    refac: rm logs

--- components/onboarding/hooks.tsx
+++ components/onboarding/hooks.tsx
@@ -1,10 +1,4 @@
-import {
-  getConnectionSourceType,
-  globalContext,
-  React,
-  useEffect,
-  useExtensionInfo,
-} from "../../deps.ts";
+import { getConnectionSourceType, React, useEffect, useExtensionInfo } from "../../deps.ts";
 import { hasConnection, useOrgUserConnectionsQuery } from "../../shared/mod.ts";
 import { usePageStack } from "../page-stack/mod.ts";
 import { BasePage, OAuthConnectionPage } from "./mod.ts";
@@ -19,26 +13,17 @@ export function useOnboarding() {
   const { pushPage } = usePageStack();
   const { extensionInfo } = useExtensionInfo();
 
-  console.log("orgUser", orgUser, isFetchingOrgUser, extensionInfo);
   useEffect(() => {
     const connectionSourceType = extensionInfo?.pageInfo
       ? getConnectionSourceType(extensionInfo.pageInfo)
       : "youtube";
 
-    if (extensionInfo?.pageInfo) {
-      console.log(
-        "getConnectionSourceType(extensionInfo.pageInfo)",
-        getConnectionSourceType(extensionInfo.pageInfo),
-      );
-    }
     const hasPageInfo = window?._truffleInitialData?.clientConfig?.IS_PROD_ENV
       ? extensionInfo?.pageInfo
       : true;
-    console.log("connectionSourceType", connectionSourceType);
     if (
       hasPageInfo && !hasConnection(orgUser, connectionSourceType) && !isFetchingOrgUser
     ) {
-      console.log("missing connection sourceType");
       pushPage(<BasePage />);
       pushPage(<OAuthConnectionPage sourceType={connectionSourceType} />);
     }
