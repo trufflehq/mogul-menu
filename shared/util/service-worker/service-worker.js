c8ef8e45ebdae9e872c7c2fca137b8be0b15aa54
Austin Fay <austin@austinfay.com>
2022-09-14 21:10:32 -0600

    chore: matching firebase deps

--- shared/util/service-worker/service-worker.js
+++ shared/util/service-worker/service-worker.js
@@ -4,10 +4,10 @@
  */
 
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js"
+  "https://www.gstatic.com/firebasejs/9.9.4/firebase-app-compat.js"
 );
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js"
+  "https://www.gstatic.com/firebasejs/9.9.4/firebase-messaging-compat.js"
 );
 
 firebase.initializeApp({
