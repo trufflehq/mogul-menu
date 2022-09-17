c9b65b0610b6e74821d54aee92b9f59f4cd3f9f5
Austin Fay <austin@austinfay.com>
2022-09-16 22:42:19 -0600

    chore: deploy

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "0.1.101",
+  version: "0.1.102",
   // name: "@truffle-dev-early-access/mogul-menu",
   // version: "0.5.6",
   // staging
