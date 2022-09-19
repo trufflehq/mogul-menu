16fde0e3f5ff818986bed1d325a6e6de5c52143a
Riley Miller <rmllr000@gmail.com>
2022-09-18 21:55:49 -0600

    chore: bump oauth package

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "0.1.109",
+  version: "0.1.110",
   // name: "@truffle-dev-early-access/mogul-menu",
   // version: "0.5.6",
   // staging
