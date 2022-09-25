cf4d739819d8fa4109d734d3355421efc186a479
Riley Miller <rmllr000@gmail.com>
2022-09-25 12:42:51 -0600

    chore: add log and bump

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,13 +1,13 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "0.1.115",
+  version: "0.1.116",
   // name: "@truffle-dev-early-access/mogul-menu",
   // version: "0.5.6",
   // staging
-  // apiUrl: "https://mycelium.staging.bio/graphql",
+  apiUrl: "https://mycelium.staging.bio/graphql",
 
   // prod
-  apiUrl: "https://mycelium.truffle.vip/graphql",
+  // apiUrl: "https://mycelium.truffle.vip/graphql",
 
   // localhost
   // apiUrl: 'http://localhost:50420/graphql'
