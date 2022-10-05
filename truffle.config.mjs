0ff2e57f462f7a2911ae21d73c850768661cd258
Riley Miller <rmllr000@gmail.com>
2022-10-05 13:14:15 -0600

    chore: bump version

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "0.1.137",
+  version: "0.1.138",
   // name: "@truffle-dev-early-access/mogul-menu",
   // version: "0.5.6",
   // staging
