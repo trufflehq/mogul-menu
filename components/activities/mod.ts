71d593bab444751012dd327c4da7a787acf3279e
Riley Miller <rmllr000@gmail.com>
2022-10-10 10:45:44 -0600

    refac: switch from context to signals

--- components/activities/mod.ts
+++ components/activities/mod.ts
@@ -1,5 +1,5 @@
-export { ActivityBannerEmbed } from './activity-banner.tsx';
-export type { ActivityBannerProps,ActivityBannerManagerProps } from './activity-banner.tsx';
-export * from './activity-banner-fragment/mod.ts';
-export * from './poll-banner/mod.ts';
-export * from './alert-banner/mod.ts';
+export { ActivityBannerEmbed } from "./activity-banner.tsx";
+export type { ActivityBannerManagerProps, ActivityBannerProps } from "./activity-banner.tsx";
+export * from "./activity-banner-fragment/mod.ts";
+export * from "./poll-banner/mod.ts";
+export * from "./alert-banner/mod.ts";
