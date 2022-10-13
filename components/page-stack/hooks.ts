4785c90ecf0db422b3a868ae50e54a92230151e9
Riley Miller <rmllr000@gmail.com>
2022-10-12 22:21:42 -0600

    refac: move page stack to signals

--- components/page-stack/hooks.ts
+++ components/page-stack/hooks.ts
@@ -1,6 +1,32 @@
-import { useContext } from "../../deps.ts";
-import { PageStackContext } from './context.ts'
+import { signal } from "../../deps.ts";
+
+export interface PageState {
+  Component: React.ReactNode;
+  isEscapeDisabled: boolean;
+}
+
+const pageStack$ = signal<PageState[]>([]);
 
 export function usePageStack() {
-  return useContext(PageStackContext);
+  return {
+    pageStack$,
+
+    pushPage: (PageComponent: React.ReactNode, isEscapeDisabled = false) => {
+      pageStack$.set((currentStack) =>
+        currentStack.concat({ Component: PageComponent, isEscapeDisabled })
+      );
+    },
+
+    peekPage: () => {
+      return pageStack$.get()[0];
+    },
+
+    popPage: () => {
+      pageStack$.set((currentStack) => currentStack.slice(0, -1));
+    },
+
+    clearPageStack: () => {
+      pageStack$.set([]);
+    },
+  };
 }
