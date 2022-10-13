3246904c5f98f7609a98675db2ba8e54015e3179
Riley Miller <rmllr000@gmail.com>
2022-10-12 22:24:17 -0600

    refac: support past predictions and active predictions

--- components/prediction-page/prediction-page.scss.js
+++ components/prediction-page/prediction-page.scss.js
@@ -18,4 +18,22 @@ export default scss`
     margin-right: 8px;
   }
 }
+
+.c-predictions-page_footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  > .error {
+    color: var(--error-red);
+  }
+}
+.c-prediction-page_empty-predictions {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
 `;
