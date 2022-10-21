903e97e689a5ed38c5c36cc03fe195de14f87803
Austin Hallock <austinhallock@gmail.com>
2022-10-21 13:35:19 -0700

    bump dist lib

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "0.2.14",
+  version: "0.2.15",
   // name: "@truffle-dev-early-access/mogul-menu",
   // version: "0.5.6",
   // staging
