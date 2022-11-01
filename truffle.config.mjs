1db51ba1a57aa90b381cca228060a5633df050b4
Austin Fay <austin@austinfay.com>
2022-11-01 11:37:51 -0600

    wip: truffle.config.mjs

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,12 +1,52 @@
+// here for syntax highlighting :)
+const gql = (strings) => strings.join();
+
 export default {
   name: "@truffle/mogul-menu",
-  version: "0.2.29",
+  version: "0.2.36",
   // name: "@truffle-dev-early-access/mogul-menu",
   // version: "0.5.6",
   // staging
   // apiUrl: "https://mycelium.staging.bio/graphql",
   // prod
-  apiUrl: "https://mycelium.truffle.vip/graphql",
+  // apiUrl: "https://mycelium.truffle.vip/graphql",
   // localhost
-  // apiUrl: 'http://localhost:50420/graphql'
+  apiUrl: "http://localhost:50330/graphql",
+
+  requestedPermissions: [
+    {
+      filters: { orgUserCounterType: { isAll: true, rank: 0 } },
+      action: "update",
+      value: true,
+    },
+  ],
+
+  installActionRel: {
+    actionPath: "@truffle/core@latest/_Action/workflow",
+    runtimeData: {
+      mode: "sequential",
+      stepActionRels: [
+        {
+          actionPath: "@truffle/core@latest/_Action/graphql",
+          runtimeData: {
+            query: gql`
+              mutation {
+                orgUserCounterTypeUpsert(
+                  input: {
+                    name: "Channel points"
+                    slug: "channel-points"
+                    decimalPlaces: 0
+                  }
+                ) {
+                  orgUserCounterType {
+                    id
+                  }
+                }
+              }
+            `,
+          },
+        },
+      ],
+    },
+  },
 };
