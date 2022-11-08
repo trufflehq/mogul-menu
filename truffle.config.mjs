6801b70d1a03bb35c5e6b5c9effd61bf6c780ffe
Riley Miller <rmllr000@gmail.com>
2022-11-07 20:27:18 -0700

    refac: use prod oauth

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/native-mogul-menu",
-  version: "0.2.30",
+  version: "0.2.31",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Mobile Menu for channel points, predictions, and activities",
   requestedPermissions: [],
