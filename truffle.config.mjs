690cf3014001302f4a74d27d790a0a6f96d2cc5c
Riley Miller <rmllr000@gmail.com>
2022-11-10 18:21:53 -0700

    fix: fix legend explodey

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/native-mogul-menu",
-  version: "0.2.41",
+  version: "0.2.43",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Mobile Menu for channel points, predictions, and activities",
   requestedPermissions: [],
