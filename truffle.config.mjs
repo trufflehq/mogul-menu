4b8027c45f86eebb6a2e98bd2a0d8bf2689ff168
Riley Miller <rmllr000@gmail.com>
2022-11-28 00:43:19 -0700

    chore: bump version

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "1.0.50",
+  version: "1.0.51",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Menu for channel points, predictions, and activities",
   requestedPermissions: [],
