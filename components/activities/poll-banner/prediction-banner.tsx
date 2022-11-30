77b39afb093b20a4f92e38407c3bb994d6aab7f5
Austin Hallock <austinhallock@gmail.com>
2022-11-29 18:19:33 -0800

    don't show NaN in activity banner

--- components/activities/poll-banner/prediction-banner.tsx
+++ components/activities/poll-banner/prediction-banner.tsx
@@ -58,9 +58,10 @@ function CurrentPrediction({ pollQuestion }: { pollQuestion: string }) {
 function PredictionEndedVoted(
   { amount, didWin = false }: { amount?: number; didWin?: boolean },
 ) {
+  const message = didWin ? `You won` : `You lost`;
   return (
     <ActivityBannerInfo
-      text={didWin ? `You won ${amount}` : `You lost ${amount}`}
+      text={amount && isNaN(amount) ? message : `${message} ${amount}`}
     >
       {amount ? <ChannelPointsIcon /> : null}
     </ActivityBannerInfo>
--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,7 +1,7 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "1.0.51",
-  apiUrl: "https://mycelium.truffle.vip/graphql",
+  version: "1.0.52",
+  apiUrl: "https://mycelium.staging.bio/graphql",
   description: "Menu for channel points, predictions, and activities",
   requestedPermissions: [],
   installActionRel: {},
