d09067b446b11ba563e2900d48216880083ce838
Riley Miller <rmllr000@gmail.com>
2022-11-30 22:15:36 -0700

    chore: bump yt chat

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/native-mogul-menu",
-  version: "0.2.46",
+  version: "0.2.47",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Mobile Menu for channel points, predictions, and activities",
   requestedPermissions: [],
