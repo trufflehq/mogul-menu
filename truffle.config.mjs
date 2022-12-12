75fb6aa6812ed5bc5a777ec8c0196d69bc78d0c3
Riley Miller <rmllr000@gmail.com>
2022-12-11 17:35:32 -0700

    chore: bump poll

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "3.0.29",
+  version: "3.0.30",
   // apiUrl: "https://mycelium.staging.bio/graphql",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Menu for channel points, predictions, and activities",
