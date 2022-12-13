2d649aa9e5d650d08563de9d149fe3ba8a16adc0
Austin Fay <austin@austinfay.com>
2022-12-13 13:39:08 -0700

    chore: invalidate first time notifications setup banner setting

--- shared/util/notifications/constants.ts
+++ shared/util/notifications/constants.ts
@@ -1,2 +1,2 @@
-export const HAS_SEEN_NOTIFICATION_SETUP_BANNER = "notifications:hasSeenSetupBanner";
+export const HAS_SEEN_NOTIFICATION_SETUP_BANNER = "notifications:hasSeenSetupBanner_1";
 export const SETUP_NOTIFICATIONS_BANNER = "setup-notifications-banner";
--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,10 +1,9 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "3.0.33",
+  version: "3.0.34",
   // apiUrl: "https://mycelium.staging.bio/graphql",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Menu for channel points, predictions, and activities",
   requestedPermissions: [],
   installActionRel: {},
 };
-
