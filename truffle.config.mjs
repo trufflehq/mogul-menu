84995d8a80bf3660d01a24227743d5763091a690
Austin Fay <austin@austinfay.com>
2022-12-15 11:03:36 -0700

    chore: bump version

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,8 +1,8 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "3.0.36",
-  apiUrl: "https://mycelium.staging.bio/graphql",
-  // apiUrl: "https://mycelium.truffle.vip/graphql",
+  version: "3.0.37",
+  // apiUrl: "https://mycelium.staging.bio/graphql",
+  apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Menu for channel points, predictions, and activities",
   requestedPermissions: [],
   installActionRel: {},
