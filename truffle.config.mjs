089358537c357a01599465878d5b47f552cf704c
Riley Miller <rmllr000@gmail.com>
2022-12-15 23:02:00 -0700

    fix: fix raid and watch party list item

--- truffle.config.mjs
+++ truffle.config.mjs
@@ -1,6 +1,6 @@
 export default {
   name: "@truffle/mogul-menu",
-  version: "3.0.40",
+  version: "3.0.41",
   // apiUrl: "https://mycelium.staging.bio/graphql",
   apiUrl: "https://mycelium.truffle.vip/graphql",
   description: "Menu for channel points, predictions, and activities",
